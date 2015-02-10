503bd293c895be098a0c889f9008d85bdabc8d8e
Pisces000221 <1786762946@qq.com>
2015-02-10 21:27:26 +0800

    单独提取出显示字幕的方法

--- 2/animation.js
+++ 2/animation.js
@@ -1,16 +1,22 @@
 var acg = acg || {};
 acg.ext = acg.ext || {};
 
+// 懒得再去修改 acg.ext.words 的实现了，干脆直接在这里写一个
+// TODO: 把 acg.ext.words 的实现变成这个
+putText = function (time, text, delay) {
+    acg.put(time, acg.text(
+        {x: 0.5, y: 0, ax: 0.5, ay: 0, fontsize: 26,
+            text: text, colour: [255, 255, 255]},
+        ['delay', delay]
+    ));
+};
+
 var initStage = function () {
     acg.put(0, acg.img('a6_title.png',
         {x: 0.5, y: 0.5, opacity: 0.0001},
         ['+', ['fade-in', 0.8], ['delay', 1.4], ['fade-out', 0.8]]
     ));
-    acg.put(0, acg.text(
-        {x: 0.5, y: 0, ax: 0.5, ay: 0, fontsize: 26,
-            text: '-= 鸭梨山大的毕业 出品 =-', colour: [255, 255, 255]},
-        ['delay', 3]
-    ));
+    putText(0, '-= 鸭梨山大的毕业 出品 =-', 3);
     for (var i = 1; i <= 5; i++) {
         acg.put(i * 4, acg.img('credits-' + i + '.png',
             {x: 0.618, y: 0.2, ax: 0.618, ay: 0, opacity: 0.0001},
@@ -31,11 +37,7 @@ var initStage = function () {
             ['scale-to', 4, 1.2]
         ]
     ));
-    acg.put(24, acg.text(
-        {x: 0.5, y: 0, ax: 0.5, ay: 0, fontsize: 26,
-            text: '-= 一模前的那些事儿 =-', colour: [255, 255, 255]},
-        ['delay', 4]
-    ));
+    putText(24, '-= 一模前的那些事儿 =-', 4);
     acg.commit();
     acg.ext.cp_enable();
     cc.director.setDisplayStats(false);
