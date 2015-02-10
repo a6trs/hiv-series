316cab5388fedc2e27898b46984a3b1d520e4f7a
Pisces000221 <1786762946@qq.com>
2015-02-10 13:44:51 +0800

    完善开始部分的画面，增加制作人员名单

--- 2/animation.js
+++ 2/animation.js
@@ -11,14 +11,27 @@ var initStage = function () {
             text: '-= 鸭梨山大的毕业 出品 =-', colour: [255, 255, 255]},
         ['delay', 3]
     ));
-    acg.put(4, acg.img('IMO_title.png',
+    for (var i = 1; i <= 5; i++) {
+        acg.put(i * 4, acg.img('credits-' + i + '.png',
+            {x: 0.618, y: 0.2, ax: 0.618, ay: 0, opacity: 0.0001},
+            ['+', ['fade-in', 0.8], ['delay', 1.4], ['fade-out', 0.8]]
+        ));
+    }
+    acg.put(24, acg.img('IMG_0046.JPG',
+        {x: 0.5, y: 0.5, opacity: 0.0001, scale: 1.5},
+        ['//',
+            ['+', ['fade-to', 0.4, 0.4 * 255], ['delay', 4], ['fade-out', 0.4]],
+            ['scale-to', 4, 1.2]
+        ]
+    ));
+    acg.put(24, acg.img('IMO_title.png',
         {x: 0.5, y: 0.5, opacity: 0.0001},
         ['//',
             ['+', ['fade-in', 0.8], ['delay', 2.4], ['fade-out', 0.8]],
             ['scale-to', 4, 1.2]
         ]
     ));
-    acg.put(4, acg.text(
+    acg.put(24, acg.text(
         {x: 0.5, y: 0, ax: 0.5, ay: 0, fontsize: 26,
             text: '-= 一模前的那些事儿 =-', colour: [255, 255, 255]},
         ['delay', 4]
